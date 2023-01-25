48d8c742796085b249b53cdb9f0978f854f29f63
Bohdan Senyk <senyk2001@gmail.com>
2023-01-25 15:05:15 +0200

    implemented utils

---
+++ src/test/java/com/example/autoserviceapp/AutoserviceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.autoserviceapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoserviceAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
